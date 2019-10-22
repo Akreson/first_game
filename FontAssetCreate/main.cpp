72997dcced765e370ef8b2478c3fd035a1374e2b
Alex Boot <gimules.but@gmail.com>
2019-10-22 20:51:12 +0300

    output one letter bitmap from FontAssetBuild

--- FontAssetCreate/main.cpp
+++ FontAssetCreate/main.cpp
@@ -7,30 +7,71 @@
 #define STB_TRUETYPE_IMPLEMENTATION 
 #include "stb_truetype.h"
 
+#define BITMAP_BYTES_PER_PIXEL 4
+
 struct entire_file
 {
-	 u32 Size;
 	 void *Content;
+	 u32 Size;
 };
 
 int
 main(int a, char **b)
 {
-	//stbtt_fontinfo font;
 
 	FILE *FileHandler = fopen("DevData//liberation-mono.regular.ttf", "rb");
 
 	if (FileHandler)
 	{
-		entire_file File;
+		entire_file TTFFile = {};
+		stbtt_fontinfo FontInfo = {};
+		float Scale;
+		int Width, Height, XOffset, YOffset;
 
 		fseek(FileHandler, 0, SEEK_END);
-		File.Size = ftell(FileHandler);
+		TTFFile.Size = ftell(FileHandler);
 		fseek(FileHandler, 0, SEEK_SET);
 
-		File.Content = malloc(File.Size);
-		fread(File.Content, File.Size, 1, FileHandler);
+		TTFFile.Content = malloc(TTFFile.Size);
+		fread(TTFFile.Content, TTFFile.Size, 1, FileHandler);
 		fclose(FileHandler);
+
+		stbtt_InitFont(&FontInfo, (u8 *)TTFFile.Content, 0);
+		Scale = stbtt_ScaleForPixelHeight(&FontInfo, 120.0f);
+		u8 *MonoBitmap = stbtt_GetCodepointBitmap(&FontInfo, 0, Scale, 'A',
+			&Width, &Height, &XOffset, &YOffset);
+		int Pitch = Width*BITMAP_BYTES_PER_PIXEL;
+		int DestBitmapSize = Height*Pitch;
+		u8 *DestMem = (u8 *)malloc(DestBitmapSize);
+
+		u8 *Source = MonoBitmap;
+		u8 *DestRow = DestMem + (Height - 1)*Pitch;
+		for (int Y = 0;
+			Y < Height;
+			++Y)
+		{
+			u32 *Dest = (u32 *)DestRow;
+			for (int X = 0;
+				X < Width;
+				++X)
+			{
+				u8 MonoValue = *Source++;
+				*Dest++ = 
+					((MonoValue << 24) |
+					(MonoValue << 16) |
+					(MonoValue << 8) |
+					(MonoValue << 0));
+			}
+
+			DestRow -= Pitch;
+		}
+
+		stbtt_FreeBitmap(MonoBitmap, 0);
+		free(TTFFile.Content);
+
+		FILE *DestFile = fopen("data//font.edg", "wb");
+		fwrite(DestMem, DestBitmapSize, 1, DestFile);
+		fclose(DestFile);
 	}
 	else
 	{
